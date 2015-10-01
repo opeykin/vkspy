e9a7ed99cd274180162377ba5a0024dec8cfa145
Alexander Opeykin <alexander.opeykin@gmail.com>
2015-10-01 03:21:53 +0300

    ids source

---
+++ src/main/kotlin/org/vkspy/IdsSource.kt
@@ -0,0 +1,8 @@
+package org.vkspy
+
+public class IdsSource
+{
+    public fun get(): List<String> {
+        return arrayListOf("opeykin", "alexey.kudinkin")
+    }
+}
--- src/main/kotlin/org/vkspy/main.kt
+++ src/main/kotlin/org/vkspy/main.kt
@@ -3,7 +3,8 @@ package org.vkspy
 fun main(args: Array<String>) {
     val accessor = VkAccessor()
     val parser = VkParser()
-    val ids = arrayListOf("opeykin", "alexey.kudinkin")
+    val idsSource = IdsSource()
+    val ids = idsSource.get()
     val response = accessor.checkOnline(ids)
     val onlineList = parser.parseOnline(response)
     println(onlineList)
