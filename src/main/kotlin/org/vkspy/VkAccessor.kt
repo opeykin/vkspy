e9c68291033a303985ce0d9041919e8fafe19712
Alexander Opeykin <alexander.opeykin@gmail.com>
2015-10-01 03:19:13 +0300

    minor code rearrange

--- src/main/kotlin/org/vkspy/VkAccessor.kt
+++ src/main/kotlin/org/vkspy/VkAccessor.kt
@@ -3,17 +3,15 @@ package org.vkspy
 public class VkAccessor {
     object Data {
         val httpClient = HttpClientWrapper()
-        val parser = VkParser()
         val apiUrl = "https://api.vk.com/method/"
     }
 
-    fun checkOnline(ids: List<String>): List<OnlineResponse> {
+    fun checkOnline(ids: List<String>): String {
         if (ids.size() > 1000)
             throw Exception("too match ids. 1000 is limit")
 
         var url = createOnlineRequestUrl(ids.join(","))
-        val response = Data.httpClient.executeGet(url)
-        return Data.parser.parseOnline(response)
+        return Data.httpClient.executeGet(url)
     }
 
     private fun createOnlineRequestUrl(id: String): String {
--- src/main/kotlin/org/vkspy/main.kt
+++ src/main/kotlin/org/vkspy/main.kt
@@ -2,7 +2,9 @@ package org.vkspy
 
 fun main(args: Array<String>) {
     val accessor = VkAccessor()
+    val parser = VkParser()
     val ids = arrayListOf("opeykin", "alexey.kudinkin")
     val response = accessor.checkOnline(ids)
-    println(response)
+    val onlineList = parser.parseOnline(response)
+    println(onlineList)
 }
