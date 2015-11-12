686f716e27c03984321189ee85b0399a98f9d9cc
Alexander Opeykin <alexander.opeykin@gmail.com>
2015-11-12 16:04:13 +0300

    basic frontend files

---
+++ frontend/resources/config.php
@@ -0,0 +1,33 @@
+<?php
+
+$config = array(
+    "db1" => array(
+        "dbname" => "alex",
+        "username" => "vkspy_stat_collector",
+        "password" => "password",
+        "host" => "localhost",
+        "port" => 5432
+    ),
+    "urls" => array(
+    "baseUrl" => "http://vkspy.info"
+    ),
+    "paths" => array(
+    "resources" => "/path/to/resources",
+    "images" => array(
+        "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+        "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+    )
+)
+);
+
+
+//defined("LIBRARY_PATH")
+//or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+//
+//defined("TEMPLATES_PATH")
+//or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+
+ini_set("error_reporting", "true");
+error_reporting(E_ALL);
+
+?>
