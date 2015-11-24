82da5bb33eff01ed9ae070568314592410ed10a5
Alexander Opeykin <alexander.opeykin@gmail.com>
2015-11-25 02:40:57 +0300

    draw report scatter chart

--- frontend/public_html/report.php
+++ frontend/public_html/report.php
@@ -1,10 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-
 <?php
 
 require('../resources/config.php');
@@ -12,7 +6,6 @@ require('util/VkSpyDb.php');
 require('util/misc.php');
 
 $uid = filter_input(INPUT_GET, "uid", FILTER_SANITIZE_STRING);
-echo "uid: " . $uid . "\n";
 $db = new VkSpyDb($config);
 
 if (!$db->hasUid($uid)) {
@@ -40,19 +33,57 @@ for ($i = 0; $i < $rowCount; $i++) {
     $first = $stat[$i];
     $last = $stat[$j - 1];
 
-    $date = parsePostgreDate($first["time"]);
+    $date = parsePostgreDate($first["time"])->getTimestamp() * 1000;
     $duration = dateIntervalInSeconds($first["time"], $last["time"]) + 60;
     $mobile = $first["mobile"];
 
     $entry = array("date" => $date, "duration" => $duration, "mobile" => $mobile);
     $arr[] = $entry;
+    $i = $j;
 }
 
 $json = json_encode($arr);
-echo $json;
+echo "<script>var dataJson = $json</script>";
 
-$pdo = null;
+$db = null;
 ?>
 
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var data = new google.visualization.DataTable();
+
+            data.addColumn('date', 'Date');
+            data.addColumn('timeofday', 'Time');
+
+            for (var i = 0; i < dataJson.length; ++i) {
+                var entry = dataJson[i];
+                var date = new Date(entry.date);
+                data.addRow([date, [date.getHours(), date.getMinutes(), date.getSeconds()]]);
+            }
+
+            var options = {
+                title: 'Online statistics',
+                hAxis: {title: 'Date'},
+                vAxis: {title: 'Time'},
+                legend: 'none'
+            };
+
+            var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+<body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
 </body>
 </html>
