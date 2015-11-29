fc40c91d417b819f36b5ec7ad990ae37eb7b49f1
Alexander Opeykin <alexander.opeykin@gmail.com>
2015-11-29 18:19:05 +0300

    some sql data preparation for testing

--- backend/db/fill.sql
+++
@@ -1,6 +0,0 @@
-insert into users values (7860407, 'Sanya', 'Opeykin', now());
-insert into users values (7328222, 'Alexey', 'Kudinkin', now());
-insert into users values (69283593, 'Matvey', 'Kotov', now());
-
-
-insert into stats (uid, mobile, app, time) values (7860407, false, 0, now());
